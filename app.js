8b84f8e97469bdab18ec4c019f814d8e4c0f67ff
surjeetmathur <153985050+surjeetmathur@users.noreply.github.com>
2024-09-19 11:36:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const userModel = require("./models/user")
+
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => {
+    res.render('index');
+});
+app.get("/read", async(req, res) => {
+    let users = await userModel.find();
+    res.render('read', { users });
+});
+app.post("/create", async(req, res) => {
+    let { name, email, image } = req.body;
+    let CreateUser = await userModel.create({
+        name,
+        email,
+        image
+    });
+    // res.send(CreateUser);
+    res.redirect('/');
+
+});
+app.get("/delete/:id", async(req, res) => {
+    let users = await userModel.findOneAndDelete({ _id: req.params.id })
+    res.redirect("/read");
+});
+app.get("/edit/:userid", async(req, res) => {
+    let users = await userModel.findOne({ _id: req.params.userid })
+    res.render('edit', { users });
+});
+app.post("/update/:usersid", async(req, res) => {
+    let { image, email, name } = req.body;
+    let users = await userModel.findOneAndUpdate({ _id: req.params.usersid }, { image, email, name }, { new: true });
+    res.redirect("/read");
+});
+
+app.listen(3000);
